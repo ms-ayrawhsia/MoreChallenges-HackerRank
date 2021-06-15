fb0db9f58d1771da6af23f0b393fb7b4804be5f9
ms-ayrawhsia <85438061+ms-ayrawhsia@users.noreply.github.com>
2021-06-15 10:35:55 +0530

    Create Day0.java

---
+++ Day0.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+        
+        
+    }
+    
+}
